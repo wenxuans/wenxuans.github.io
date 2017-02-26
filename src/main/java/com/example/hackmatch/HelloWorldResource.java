9d49296794b24ebf00abeb599dab831916f80a05
arushibandi <bandia.gos@gmail.com>
2017-02-26 11:17:02 -0500

    post import

--- src/main/java/com/example/hackmatch/HelloWorldResource.java
+++ src/main/java/com/example/hackmatch/HelloWorldResource.java
@@ -3,6 +3,7 @@ package com.example.hackmatch;
 //import com.example.hackmatch.Greeting;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;

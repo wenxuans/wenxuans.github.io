2cd2408cea2867f2031489c6f99b973aae2258c2
arushibandi <bandia.gos@gmail.com>
2017-02-25 19:20:22 -0500

    did the maven stuff

---
+++ hackmatch-slackapp/src/main/java/com/example/hackmatch/HackMatchApplication.java
@@ -0,0 +1,29 @@
+package com.example.hackmatch;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HackMatchApplication extends Application<HackMatchConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new HackMatchApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "HackMatch";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<HackMatchConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final HackMatchConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ hackmatch-slackapp/src/main/java/com/example/hackmatch/HackMatchConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.hackmatch;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class HackMatchConfiguration extends Configuration {
+    // TODO: implement service configuration
+}

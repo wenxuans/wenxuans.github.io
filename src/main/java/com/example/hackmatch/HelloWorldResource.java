56017b651191fde85a9b9692317006e084ffff2e
arushibandi <bandia.gos@gmail.com>
2017-02-26 11:22:38 -0500

    put

--- src/main/java/com/example/hackmatch/HelloWorldResource.java
+++ src/main/java/com/example/hackmatch/HelloWorldResource.java
@@ -3,7 +3,7 @@ package com.example.hackmatch;
 //import com.example.hackmatch.Greeting;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -18,7 +18,7 @@ public class HelloWorldResource {
     public HelloWorldResource() {
 	
     }
-    @POST
+    @PUT
     public Greeting sayHello() {
 	return new Greeting();
     }
